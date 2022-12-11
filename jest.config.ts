9db2b19c0e152521148825e72a8524d381efb5ad
danielpqb <danielpqb@gmail.com>
2022-12-11 20:38:33 -0300

    Implementing jest config

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const config = {
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
+};
+
+export default config;
